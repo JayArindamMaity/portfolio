c67255d029d11da171c70306d3a4bb184db6fd6a
JayArindamMaity <maityjayarindam@gmail.com>
2025-02-18 12:48:24 +0530

    changes

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -8,6 +8,18 @@ const Contact: React.FC = () => {
         <div className="contact-title">
           Contact Me:
         </div>
+        <div className="contact-paragraph">
+          Hey! you wish to contact me or know more about me here are some of the ways/links to do so...
+        </div>
+        <div className="">
+          <ul>
+            <li>Instagram: </li>
+            <li>Twitter: </li>
+            <li>GitHub: </li>
+            <li>Linkedin: </li>
+            <li>Mail: </li>
+          </ul>
+        </div>
     </div>
     </>
   );
